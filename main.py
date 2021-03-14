957096206d4dfdc8108335ad9eb86b469906c281
Hotabuchic <58589838+Hotabuchic@users.noreply.github.com>
2021-03-14 15:01:19 +0300

    fourth task

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
 import sys
-from PyQt5.uic import loadUi
+
 import requests
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
 
 
 class MyWidget(QMainWindow):
-    def __init__(self, ):
+    def __init__(self):
         super().__init__()
         loadUi('qt.ui', self)
         self.set_image()
@@ -14,12 +16,25 @@ class MyWidget(QMainWindow):
 
     def set_image(self):
         l1, l2, spn = self.longitude.value(), self.latitude.value(), self.spn.value()
+        mapp = "map"
+        if self.mapp.currentText() == "Спутник":
+            mapp = "sat"
+        elif self.mapp.currentText() == "Гибрид":
+            mapp = "sat,skl"
         req = requests.get(
-            f"http://static-maps.yandex.ru/1.x/?ll={l1},{l2}&spn={spn},{spn}&l=map")
+            f"http://static-maps.yandex.ru/1.x/?ll={l1},{l2}&spn={spn},{spn}&l={mapp}")
         file = open('temp.png', mode='wb')
         file.write(req.content)
         file.close()
         self.label.setPixmap(QPixmap('temp.png'))
+
+    # def keyPressEvent(self, event):
+    #     if event.key() == Qt.Key_PageUp:
+    #         self.spn.setValue(self.spn.value() + 0.04)
+    #         self.set_image()
+    #     elif event.key() == Qt.Key_PageDown:
+    #         self.spn.setValue(self.spn.value() - 0.04)
+    #         self.set_image()
 
 
 if __name__ == '__main__':
@@ -27,4 +42,3 @@ if __name__ == '__main__':
     my = MyWidget()
     my.show()
     sys.exit(app.exec_())
-
