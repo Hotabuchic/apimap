4bd6a8b3f14ee35408260f6fc1f7b4f8ae345f3d
Hotabuchic <58589838+Hotabuchic@users.noreply.github.com>
2021-03-31 13:38:23 +0300

    seventh task

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 
 import requests
-from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
@@ -14,20 +13,31 @@ class MyWidget(QMainWindow):
         self.key = "40d1649f-0493-4b70-98ba-98533de7710b"
         self.set_image()
         self.btn.clicked.connect(self.set_image)
+        self.cancel.clicked.connect(self.cancel_search)
 
     def set_image(self):
         response = requests.get(
             f"http://geocode-maps.yandex.ru/1.x/?apikey={self.key}&geocode={self.search.text()}&format=json").json()
         toponym = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-        l1, l2 = toponym["Point"]["pos"].split()
-        spn = self.spn.value()
-        mapp = "map"
-        if self.mapp.currentText() == "Спутник":
-            mapp = "sat"
-        elif self.mapp.currentText() == "Гибрид":
-            mapp = "sat,skl"
+        self.l1, self.l2 = toponym["Point"]["pos"].split()
+        self.spn = self.spnn.value()
+        self.mapp = "map"
+        if self.mappp.currentText() == "Спутник":
+            self.mapp = "sat"
+        elif self.mappp.currentText() == "Гибрид":
+            self.mapp = "sat,skl"
         req = requests.get(
-            f"http://static-maps.yandex.ru/1.x/?ll={l1},{l2}&spn={spn},{spn}&l={mapp}&pt={l1},{l2},org")
+            f"http://static-maps.yandex.ru/1.x/?ll={self.l1},{self.l2}&"
+            f"spn={self.spn},{self.spn}&l={self.mapp}&pt={self.l1},{self.l2},org")
+        file = open('temp.png', mode='wb')
+        file.write(req.content)
+        file.close()
+        self.label.setPixmap(QPixmap('temp.png'))
+
+    def cancel_search(self):
+        req = requests.get(
+            f"http://static-maps.yandex.ru/1.x/?ll={self.l1},{self.l2}&"
+            f"spn={self.spn},{self.spn}&l={self.mapp}")
         file = open('temp.png', mode='wb')
         file.write(req.content)
         file.close()
